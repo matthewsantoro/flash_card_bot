62c4c8bb3e51f0eebc9cccd718c5515412409fc2
Matthew Santoro <elektroforst88@gmail.com>
2024-12-17 23:04:52 +0300

    add models user

--- models/models.py
+++ models/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey,DateTime, func
+from sqlalchemy import Column, Integer, String, ForeignKey,DateTime, func,BOOLEAN
 from sqlalchemy.orm import relationship, Mapped, mapped_column, DeclarativeBase
 
 
@@ -20,16 +20,27 @@ class Card(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     question: Mapped[str] = mapped_column(String(1000), nullable=False)
     answer: Mapped[str] = mapped_column(String(1000), nullable=False)
-    category_id: Mapped[int] = mapped_column(ForeignKey('categories.id'))  # Внешний ключ для категории
-    set_id: Mapped[int] = mapped_column(ForeignKey('sets.id'))  # Внешний ключ для набора
+    category_id: Mapped[int] = mapped_column(ForeignKey('categories.id')) 
+    set_id: Mapped[int] = mapped_column(ForeignKey('sets.id')) 
 
     category: Mapped["Category"] = relationship("Category", back_populates="cards")
     set: Mapped["Set"] = relationship("Set", back_populates="cards")
 
 class Set(Base):
-    __tablename__ = 'sets'  # Исправлено на __tablename__
+    __tablename__ = 'sets' 
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(String(150), unique=True, nullable=False)
+    private: Mapped[bool] = mapped_column(BOOLEAN, default=True)
+    creator_id: Mapped[int] = mapped_column(ForeignKey('users.id')) 
 
-    cards: Mapped[list["Card"]] = relationship("Card", back_populates="set")  # Исправлено на "set"
+    card: Mapped[list["Card"]] = relationship("Card", back_populates="set")  
+    creator: Mapped["User"] = relationship("User", back_populates="set")
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(Integer,primary_key=True)
+    name: Mapped[str] = mapped_column(String(150), unique=False, nullable=False)
+
+    set: Mapped["Set"] = relationship("Set", back_populates="creator")
