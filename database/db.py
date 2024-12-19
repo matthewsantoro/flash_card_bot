439161a0d4d10546f818288e130fb1594c9b920a
matthewsantoro <elektroforst88@gmail.com>
2024-12-19 18:45:50 +0300

    rewrite db init and metods

--- database/db.py
+++ database/db.py
@@ -1,23 +1,23 @@
 import logging
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker
+from os import getenv
+from sqlalchemy import select
+
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from models.models import  Card, Category, Base, User, Set
 
 
-
-
 class Database:
-    def __init__(self, db_config):
-        self.db_config = db_config
-        self.engine = None
-        self.SessionLocal = None
+    def __init__(self):
+        self.db_host = getenv('DB_HOST')
+        self.db_user = getenv('DB_USER')
+        self.db_password = getenv('DB_PASSWORD')
+        self.db_port = getenv('DB_PORT')
+        self.db_database = getenv('DB_DATABASE')
+        self.connect = f"postgresql+asyncpg://{self.db_user}:{self.db_password}@{self.db_host}:{self.db_port}/{self.db_database}"
+        self.async_engine = create_async_engine(self.connect, echo=True)
+        self.Session = async_sessionmaker(bind=self.async_engine, class_=AsyncSession)
 
-    async def connect(self):
-        """Создает асинхронный движок и сессию."""
-        DATABASE_URL = f"postgresql+asyncpg://{self.db_config['USER']}:{self.db_config['PASSWORD']}@{self.db_config['HOST']}:{self.db_config['PORT']}/{self.db_config['DATABASE']}"
-        self.engine = create_async_engine(DATABASE_URL, echo=True)
-        self.SessionLocal = sessionmaker(bind=self.engine, class_=AsyncSession, expire_on_commit=False)
-        logging.info("Подключение к базе данных успешно установлено.")
+
 
     async def close(self):
         """Закрывает соединение с базой данных."""
@@ -26,39 +26,46 @@ class Database:
 
     async def setup(self):
         """Создает необходимые таблицы в базе данных, если они не существуют."""
-        async with self.engine.begin() as conn:
+        async with self.async_engine.begin() as conn:
             await conn.run_sync(Base.metadata.create_all)
             logging.info("Таблицы успешно созданы или уже существуют.")
 
-    async def get_session(self):
-        """Возвращает новую сессию для работы с базой данных."""
-        async with self.SessionLocal() as session:
-            yield session
     
-    async def add_category(self, session: AsyncSession, name: str):
-        new_category = Category(name=name)
-        session.add(new_category)
-        await session.commit()
+    async def add_category(self, name: str):
+        async with self.Session() as session:
+            new_category = Category(name=name)
+            session.add(new_category)
+            await session.commit()
         return new_category
 
-    async def add_card(session: AsyncSession, question: str, answer: str, category_id: int):
-        new_card = Card(question=question, answer=answer, category_id=category_id)
-        session.add(new_card)
-        await session.commit()
+    async def add_card(self, question: str, answer: str, category_id: int):
+        async with self.Session() as session:
+            new_card = Card(question=question, answer=answer, category_id=category_id)
+            session.add(new_card)
+            await session.commit()
         return new_card
     
-    async def add_user(session: AsyncSession, user_id: int, name: str ) -> User:
-        new_user = User(name=name, id=user_id)
-        session.add(new_user)
-        await session.commit()
-        return new_user
+    async def add_user(self, user_id: int, name: str ) -> User:
+        async with self.Session() as session:
+            new_user = User(name=name, id=user_id)
+            session.add(new_user)
+            await session.commit()
     
-    async def user_exists(session: AsyncSession, user_id: int): 
-        return session.query(User).filter(User.id == user_id).first() is not None
+    async def get_user_by_id(self, user_id: int) -> User:
+        async with self.Session() as session:
+            result = await session.execute(select(User).where(User.id == user_id))
+            return result.scalars().first()
     
-    async def add_set(session: AsyncSession, name: str, creator: User, private: bool = True ) -> Set:
-        new_set = Set(name=name, creator_id=creator, private=private)
-        session.add(new_set)
-        await session.commit()
+
+    async def user_exists(self, user_id: int): 
+        async with self.Session() as session:
+            result = await session.execute(select(User).where(User.id == user_id))
+            return result.scalars().first() is not None
+    
+    async def add_set(self, name: str, creator_id: int, private: bool = True ) -> Set:
+        async with self.Session() as session:
+            new_set = Set(name=name, creator_id=creator_id, private=private)
+            session.add(new_set)
+            await session.commit()
         return new_set
    
