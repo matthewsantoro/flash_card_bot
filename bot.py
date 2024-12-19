72cca246a5b630020091419c291a190acd226f79
matthewsantoro <elektroforst88@gmail.com>
2024-12-19 11:02:31 +0300

    refractor

--- bot.py
+++ bot.py
@@ -7,25 +7,18 @@ from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, html
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart,Command
-from aiogram.types import Message
+
 from handlers import start
 
 from .config import DB_CONFIG, BOT_TOKEN
-
-
 from database.db import Database
 
 load_dotenv()
-
-
 
 dp = Dispatcher()
 db = Database(DB_CONFIG)
 
 async def main() -> None:
-    # Initialize Bot instance with default bot properties which will be passed to all API calls
-    
     bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     dp.include_routers(start.router)
     await db.on_startup()
--- handlers/start.py
+++ handlers/start.py
@@ -1,7 +1,6 @@
 from aiogram import Router, F
 from aiogram.filters import Command
-from aiogram.types import Message, ReplyKeyboardRemove
-from database.db import Database
+from aiogram.types import Message
 from bot import db
 
 router = Router()  # [1]
@@ -18,3 +17,12 @@ async def cmd_start(message: Message):
         else:
             await message.reply("Привет снова! Вы уже зарегистрированы.")
 
+@router.message(Command("add"))
+async def add_category_handler(message: Message, command):
+    category_name = command.args # Получаем аргументы команды
+    if category_name:
+        async for session in  db.get_session():
+            await db.add_category(session, category_name)
+            await message.reply(f"Категория '{category_name}' добавлена.")
+    else:
+        await message.reply("Пожалуйста, укажите название категории после команды.")
