ade33ccadaa1664998f0cc6a3610cc4afe636b4b
matthewsantoro <elektroforst88@gmail.com>
2024-12-25 18:45:47 +0300

    add card keyboard

---
+++ keyboards/cards.py
@@ -0,0 +1,14 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+
+async def finish_card() -> InlineKeyboardMarkup:
+    builder = InlineKeyboardBuilder()
+    builder.add(InlineKeyboardButton(
+        text=f"Добавить еще карточку",
+        callback_data="card_new"))
+    builder.add(InlineKeyboardButton(
+        text=f"Добавить еще карточку",
+        callback_data="card_finish"))
+    builder.adjust(2) 
+    return builder.as_markup()
