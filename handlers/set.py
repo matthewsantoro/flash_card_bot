3ebc559dc11559316befb8db8c5e5310aad4dda1
matthewsantoro <elektroforst88@gmail.com>
2025-01-13 20:31:18 +0300

    re write set add

--- handlers/set.py
+++ handlers/set.py
@@ -2,7 +2,8 @@ from aiogram import Bot, Router, F
 from aiogram.types import Message, CallbackQuery
 from database.db import Database
 from handlers import main_menu
-from states.set_state import SetState
+from handlers.cards import show_card
+from states.cards import CardState
 from aiogram.filters import Command, StateFilter
 from aiogram.fsm.context import FSMContext
 from keyboards.sets import create_sets_keyboard
@@ -11,6 +12,15 @@ router = Router()
 
 db = Database()
 
-@router.callback_query(F.data.startswith("set_"), StateFilter(SetState.choose_set))
-async def choosing_set(callback: CallbackQuery, state: FSMContext):
-    pass
+@router.message(StateFilter(CardState.add_set))
+async def adding_collection(message: Message, state: FSMContext, bot: Bot):
+    user_id = message.from_user.id 
+    data = await state.get_data()
+    collection = await db.add_set(name=message.text, creator_id=user_id)
+    await state.update_data(set_id=collection.id)
+    await state.set_state(CardState.view_card)
+    clb = data["msg_callback"]
+    await message.delete()
+    await show_card(msg=clb.message, index=None, cards=None)
+    
+    
