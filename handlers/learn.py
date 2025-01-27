0c7c15213f482edfeafae8ed5eaaec64174eddfe
matthewsantoro <elektroforst88@gmail.com>
2025-01-27 17:53:13 +0300

    add new logic

--- handlers/learn.py
+++ handlers/learn.py
@@ -11,6 +11,7 @@ from models.models import Card, Deck
 from states.cards import CardState
 from aiogram.fsm.context import FSMContext
 from aiogram.filters import StateFilter
+from datetime import datetime
 
 router = Router()
 
@@ -31,7 +32,10 @@ async def learnig_collections(callback: CallbackQuery, state: FSMContext):
         await callback.message.edit_text(f'Карточек на изучение: {len(study_cards)}', reply_markup=keyboard)
     else:
         keyboard = await create_start_study_keyboard(empty=True)
-        await callback.message.edit_text(f'У вас нет карточек для узучения', reply_markup=keyboard)
+        text = f'У вас нет карточек для узучения'
+        time : datetime= await db.get_next_time_to_learn()
+        if time: text+=f'\n следущее изучение будет {time.strftime("%d-%m-%Y %H:%M:%S")}'
+        await callback.message.edit_text(text=text, reply_markup=keyboard)
 
 
 @router.callback_query(F.data == 'learn_back', StateFilter(CardState.training))
@@ -47,7 +51,7 @@ async def go_back_card_view(callback: CallbackQuery, state: FSMContext):
 async def startig_learn(callback: CallbackQuery, state: FSMContext):
     data = await state.get_data()
     study_cards = data['study_cards']    
-    await show_study_card(callback.message, cards=study_cards[0], state=state)
+    await show_study_card(callback.message, study_card=study_cards[0], state=state)
     
 
 async def show_study_card(msg: Message, study_card: Optional[Card], state: FSMContext):
@@ -66,21 +70,21 @@ async def checking_result(callback: CallbackQuery, state: FSMContext):
     card_id = int(callback.data.split(":")[1])
     is_sucseed = bool(int(callback.data.split(":")[2]))
     data = await state.get_data()
-    study_cards= data['study_card']
+    study_cards= data['study_cards']
     study_card: Card = study_cards.popleft()
-    if study_card.level == 1 and not is_sucseed:
+    if study_card.level_id == 1 and not is_sucseed:
         await insert_random(sequence=study_cards, element=study_card)
     else: 
          await db.update_level_card(action_successful=is_sucseed, card_id=card_id)
     
     await state.update_data(study_cards=study_cards)
     if study_cards:        
-        await show_study_card(callback.message, cards=study_cards, state=state)
+        await show_study_card(callback.message, study_card=study_cards[0], state=state)
     else: 
         cards = data['cards']    
         card = data['card']
         index = cards.index(card)
-        await state.set_data(CardState.view_card)
+        await state.set_state(CardState.view_card)
         await show_card(callback.message, index=index, cards=cards, state=state)
         await callback.answer(text="Отлично, насегодня вы все изучили", show_alert=True)
 
